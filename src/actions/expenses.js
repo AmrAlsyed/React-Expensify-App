9f7c28aa4277db75043a5be71252dc1cd58ca101
AmrAlsyed <Xaxeth@gmail.com>
2024-06-25 14:14:01 +0300

    Added support for firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,16 +1,30 @@
 import { v4 as uuid } from 'uuid';
-
+import { database, push, ref, get } from '../firebase/firebase';
 // Add expense Action Generator
-export const addExpense = ({description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
-    expense: {
-        id: uuid(),
-        description,
-        note,
-        amount,
-        createdAt
-    }
+    expense
 })
+
+export const startAddExpense = (expenseData = {}) => {
+    return (dispatch) => {
+        const {
+            description = '',
+            note = '',
+            amount = 0,
+            createdAt = 0
+        } = expenseData
+
+        const expense = {description, note, amount, createdAt}
+
+        push(ref(database, 'expenses'), expense).then((ref) => {
+            dispatch(addExpense({
+                id: ref.key,
+                ...expense
+            }))
+        })
+    }
+}
 
 export const removeExpense = ({id}) => ({
     type: 'REMOVE_EXPENSE',
@@ -22,3 +36,26 @@ export const editExpense= (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+})
+
+export const startSetExpenses = () => {
+    return async (dispatch) => {
+      const snapshot =  await get(ref(database, 'expenses'))
+      const expenses = [];
+      snapshot.forEach((childSnapShot) => {
+          expenses.push({
+              id: childSnapShot.key,
+              ...childSnapShot.val()
+          })
+      })
+      dispatch(setExpenses(expenses))
+      
+        
+    }
+}
---
+++ src/firebase/firebase.js
@@ -0,0 +1,43 @@
+// import * as firebase from 'firebase'
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, update, remove, get, onValue, off, push, onChildAdded } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAle97NZICgrSa8Fs6vZJ0O-9hu2SLKJ3Y",
+    authDomain: "expensify-f1094.firebaseapp.com",
+    databaseURL: "https://expensify-f1094-default-rtdb.firebaseio.com",
+    projectId: "expensify-f1094",
+    storageBucket: "expensify-f1094.appspot.com",
+    messagingSenderId: "758753185685",
+    appId: "1:758753185685:web:5205633b409c4c5361709c"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+
+  
+
+  export {app , push, database, ref, get}
+
+  
+  // onChildAdded(ref(database, 'expenses'), (snapshot) => {
+  //   console.log(snapshot.key, snapshot.val())
+  // })
+
+  // // onValue(ref(database, 'expenses'), (snapshot) => {
+  // //   const expenses = []
+
+  // //   snapshot.forEach((childSnapshop) => {
+  // //     expenses.push({
+  // //       id: childSnapshop.key,
+  // //       ...childSnapshop.val()
+  // //     })
+  // //   })
+  // //   console.log(expenses)
+  // // })
+  // push(ref(database, 'expenses'), {
+  //   description: 'Food',
+  //   note: '',
+  //   amount: 1200,
+  //   createdAt: 3044002
+  // })
